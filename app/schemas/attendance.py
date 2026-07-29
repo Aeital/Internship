b266da229b4ec4a169482e9967f26fe44d567eb5
Ayetal <ayetalhassan612@gmail.com>
2026-07-29 12:07:24 +0000

    Jul 29, 2026, 5:07 PM

---
+++ app/schemas/attendance.py
@@ -0,0 +1,26 @@
+from datetime import date, time
+from pydantic import BaseModel
+from app.models.models import AttendanceStatus
+class AttendanceBase(BaseModel):
+    emp_id: int
+    att_date: date
+    check_in: time | None = None
+    check_out: time | None = None
+    att_status: AttendanceStatus = AttendanceStatus.PRESENT
+
+
+class AttendanceCreate(AttendanceBase):
+    pass
+
+
+class AttendanceUpdate(BaseModel):
+    check_in: time | None = None
+    check_out: time | None = None
+    att_status: AttendanceStatus | None = None
+
+
+class AttendanceResponse(AttendanceBase):
+    att_id: int
+
+    class Config:
+        from_attributes = True
