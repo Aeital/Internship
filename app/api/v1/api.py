2a885955c92e3ac23aa4f07069df345fe9d4eab9
Ayetal <ayetalhassan612@gmail.com>
2026-07-29 12:09:50 +0000

    Jul 29, 2026, 5:09 PM

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, employees, departments
-
+from app.api.v1.endpoints import auth, employees, departments, attendance
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(employees.router, prefix="/employees", tags=["Employees"])
 api_router.include_router(departments.router, prefix="/departments", tags=["Departments"])
+api_router.include_router(attendance.router, prefix="/attendance", tags=["Attendance"])
