65b88253573e505684a81ff3dd42b3118680d941
Ayetal <ayetalhassan612@gmail.com>
2026-08-03 11:42:01 +0000

    Aug 3, 2026, 4:42 PM

---
+++ ems_frontend/src/api/axiosClient.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+// Attach JWT token automatically to every outgoing request
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosClient;
