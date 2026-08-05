2eb66c8eae9e847a02e7317e12f01d0cd6f5e72a
Ayetal <ayetalhassan612@gmail.com>
2026-08-05 05:09:52 +0000

    Aug 5, 2026, 10:09 AM

--- ems_frontend/src/api/authApi.js
+++
@@ -1,6 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const login = async (email, password) => {
-  const response = await axiosClient.post("/auth/login", { email, password });
-  return response.data; // { access_token, token_type }
-};
