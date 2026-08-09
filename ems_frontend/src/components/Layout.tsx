626c3f541455a5ef3c3ac955a80dc88095d3e45e
Ayetal <ayetalhassan612@gmail.com>
2026-08-09 17:04:43 +0000

    Aug 9, 2026, 10:04 PM

--- ems_frontend/src/components/Layout.tsx
+++ ems_frontend/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router'
 import { useAuth } from '@/contexts/AuthContext'
+import netsolLogo from '@/imports/image.png'
 import {
   LayoutDashboard,
   Users,
