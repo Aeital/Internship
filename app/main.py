37517f533025d75aa0d9df008c5fe67591424247
Ayetal <ayetalhassan612@gmail.com>
2026-08-10 06:03:48 +0000

    Aug 10, 2026, 11:03 AM

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title=settings.PROJECT_NAME)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8443"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
